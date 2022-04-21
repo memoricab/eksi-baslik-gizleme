363437d9b41de50e9479ea47701d99be68c14b11
celenmeh@gmail.com <celenmeh@gmail.com>
2022-04-21 22:29:09 +0300

    Improve naming

--- background.js
+++ background.js
@@ -1,60 +1,51 @@
-const STORED_TITLES = "storedTitles";
+const STORED_TOPICS = "storedTopics";
 const CONTEXT_MENU = {
   title: "Başlığı Gizle",
   contexts: ["link"],
   documentUrlPatterns: ["http://localhost/*", "https://eksisozluk.com/**"],
   id: "998",
 };
-const STORED_TITLES_DICT_INITIAL_DECLARATION = {
-  [STORED_TITLES]: JSON.stringify([]),
+const STORED_TOPICS_DICT_INITIAL_DECLARATION = {
+  [STORED_TOPICS]: JSON.stringify([]),
 };
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set(STORED_TITLES_DICT_INITIAL_DECLARATION);
+  chrome.storage.local.set(STORED_TOPICS_DICT_INITIAL_DECLARATION);
   chrome.contextMenus.create(CONTEXT_MENU);
 });
 
-var handleTitleStorage = async ({ linkUrl }) => {
-  var createdTitle = await createNewTitle(linkUrl);
-  saveTitle(createdTitle);
+var handleTopicStorage = async ({ linkUrl }) => {
+  var createdTopic = await createNewTopic(linkUrl);
+  saveTopic(createdTopic);
 };
 
-var createNewTitle = async (titleUrl) => {
-  const url = new URL(titleUrl);
+var createNewTopic = async (topicUrl) => {
+  const url = new URL(topicUrl);
   return {
-    id: extractTitleIdFromUrl(url),
-    url: removeQueryFromTitleUrl(titleUrl),
-    self: extractTitleFromUrl(url),
+    id: extractTopicIdFromUrl(url),
+    url: removeQueryFromTopicUrl(topicUrl),
+    self: extractTopicFromUrl(url),
   };
 };
 
-const extractTitleIdFromUrl = ({ pathname }) => {
+const extractTopicIdFromUrl = ({ pathname }) => {
   return pathname.substring(pathname.indexOf("--") + 2);
 };
 
-const extractTitleFromUrl = ({ pathname }) => {
+const extractTopicFromUrl = ({ pathname }) => {
   return pathname.substring(pathname.indexOf("/") + 1).split("--")[0];
 };
 
-const removeQueryFromTitleUrl = (url) => {
+const removeQueryFromTopicUrl = (url) => {
   return url.split("?")[0];
 };
 
-const saveTitle = async (title) => {
-  var { storedTitles } = await chrome.storage.local.get([STORED_TITLES]);
-  var titles = JSON.parse(storedTitles);
-  if (isTitleAlreadyExists(title.id, titles)) return;
-
-  titles.push(title);
-  storedTitles = JSON.stringify(titles);
-  chrome.storage.local.set({ [STORED_TITLES]: storedTitles });
-};
-
-const isTitleAlreadyExists = (titleId, titles) => {
-  for (var title of titles) {
-    if (Object.values(title).includes(titleId)) return true;
-  }
-  return false;
+const saveTopic = async (topic) => {
+  var { storedTopics } = await chrome.storage.local.get([STORED_TOPICS]);
+  var topics = JSON.parse(storedTopics);
+  topic.push(topic);
+  storedTopics = JSON.stringify(topics);
+  chrome.storage.local.set({ [STORED_TOPICS]: storedTopics });
 };
 
 chrome.contextMenus.onClicked.addListener((link, { id }) => {
@@ -66,12 +57,11 @@ chrome.contextMenus.onClicked.addListener((link, { id }) => {
     },
     () => {}
   );
-  //handleTitleStorage(link);
+  //handleTopicStorage(link);
 });
 
 function removeTopicFromView({ linkUrl }) {
   var topicList = document.getElementsByClassName("topic-list partial")[0];
-  console.log(topicList);
 
   for (var topic of topicList.children) {
     var topicAnchor = topic.getElementsByTagName("a")[0];
