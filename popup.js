cc93c46ce9385e135128117a4802364b2b50ac0a
celenmeh@gmail.com <celenmeh@gmail.com>
2022-04-23 01:38:48 +0300

    Add popup logic to show topics

--- popup.js
+++ popup.js
@@ -1,4 +1,57 @@
-document.getElementById("hello").onclick = async function () {
-  var { storedTopics } = await chrome.storage.local.get(["storedTopics"]);
-  console.log(storedTopics);
+var list = document.getElementById("eksi-baslik-gizle-list");
+
+const initTopics = async () => {
+  var { storedTopics } = await chrome.storage.local.get("storedTopics");
+  var topics = JSON.parse(storedTopics);
+  for (var topic of topics) {
+    var row = document.createElement("div");
+    row.id = topic.id;
+
+    var col1 = createACol();
+    var col2 = createACol();
+
+    var anchor = createTopicAnchor(topic);
+    col1.appendChild(anchor);
+
+    var deleteBtn = createDeleteBtn();
+    col2.appendChild(deleteBtn);
+
+    row.appendChild(col1);
+    row.appendChild(col2);
+
+    list.appendChild(row);
+  }
+
+  function createTopicAnchor(topic) {
+    var anchor = document.createElement("a");
+    anchor.appendChild(document.createTextNode(topic.self));
+    anchor.href = topic.url;
+    return anchor;
+  }
+
+  function createACol() {
+    var col = document.createElement("div");
+    col.classList.add("column");
+    return col;
+  }
+
+  function createDeleteBtn() {
+    var deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "sil";
+    deleteBtn.onclick = (e) => deleteTopic(e);
+    return deleteBtn;
+  }
+
+  function deleteTopic(e) {
+    var topicRow = e.srcElement.parentElement.parentElement;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === topicRow.id) {
+        topicRow.remove();
+        topics.splice(i, 1);
+        chrome.storage.local.set({ storedTopics: JSON.stringify(topics) });
+      }
+    }
+  }
 };
+
+initTopics();
